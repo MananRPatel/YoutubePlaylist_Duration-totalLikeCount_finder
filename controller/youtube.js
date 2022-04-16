9d51f2e2f9e7b24b7008cd63fea91ff365625c47
MananRPatel <mananrpatel7144@gmail.com>
2022-04-16 17:26:40 +0530

    use the oops conscepts in this project

--- controller/youtube.js
+++ controller/youtube.js
@@ -3,107 +3,125 @@ require("dotenv").config;
 const lock = require("async-mutex");
 const apimutex = new lock.Mutex();
 const countmutex = new lock.Mutex();
-let mainResponse;
 
-let totalDurationOfVideo = 0;
-let totalLikeCount = 0;
-let totalVideo = 0;
-let videoInPlaylist = 0;
+class Youtube {
+  Youtube() {
+    this.mainResponse = 0;
+    this.totalDurationOfVideo = 0;
+    this.totalLikeCount = 0;
+    this.totalVideo = 0;
+    this.videoInPlaylist = 0;
+  }
 
-getVideoTimeline = (time) => {
-  const date = ["H", "M", "S"];
-  const timer = [0, 0, 0, 0];
+  getVideoTimeline = (time) => {
+    const date = ["H", "M", "S"];
+    const timer = [0, 0, 0, 0];
 
-  let D = time.indexOf("D");
-  if (D != -1) timer[0] = +time.slice(0 + 1, D);
+    let D = time.indexOf("D");
+    if (D != -1) timer[0] = +time.slice(0 + 1, D);
 
-  (prevIndex = time.indexOf("T")), (nextIndex = 0);
+    let prevIndex = time.indexOf("T"),
+      nextIndex = 0;
 
-  for (let i = 0; i < date.length; i++) {
-    nextIndex = time.indexOf(date[i]);
-    if (nextIndex !== -1) {
-      timer[i + 1] = +time.slice(prevIndex + 1, nextIndex);
-      prevIndex = nextIndex;
+    for (let i = 0; i < date.length; i++) {
+      nextIndex = time.indexOf(date[i]);
+      if (nextIndex !== -1) {
+        timer[i + 1] = +time.slice(prevIndex + 1, nextIndex);
+        prevIndex = nextIndex;
+      }
     }
-  }
-  return (
-    24 * 60 * 60 * timer[0] + 60 * 60 * timer[1] + 60 * timer[2] + timer[3]
-  );
-};
-
-const getVideoInfo = async (id) => {
-  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&fields=items(contentDetails(duration)),items(statistics(likeCount))&key=${process.env.YOUTUBE_API}`;
-
-  try {
-    const response = await axios.get(url);
-    const itm = response.data.items;
-    if (+itm.length == 0) {
-      await countmutex.runExclusive(() => {
-        totalVideo--;
-      });
-      return;
-    }
-
-    await apimutex.runExclusive(() => {
-      totalDurationOfVideo += getVideoTimeline(itm[0].contentDetails.duration);
-      totalLikeCount += +itm[0].statistics.likeCount;
-      countmutex.runExclusive(() => {
-        totalVideo--;
-        if (totalVideo == 0) mainResponse.send(statisticsOfVideo());
-      });
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-const getPlaylistsInfo = async (id, pageToken = null) => {
-  let currentToken = "";
-  if (pageToken != null) {
-    currentToken = `pageToken=${pageToken}`;
-  }
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?${currentToken}&part=snippet&maxResults=20&playlistId=${id}&key=${process.env.YOUTUBE_API}&fields=items(snippet(resourceId)),nextPageToken,prevPageToken,pageInfo`;
-  try {
-    const response = await axios.get(url);
-    if (response.statusCode == 400) throw new Error(response.error);
-    const nextPageToken = await response.data.nextPageToken;
-    const itm = response.data.items;
-
-    if (pageToken == null)
-      countmutex.runExclusive(() => {
-        totalVideo = +response.data.pageInfo.totalResults;
-        videoInPlaylist = totalVideo;
-      });
-
-    for (let index = 0; index < itm.length; index++) {
-      getVideoInfo(itm[index].snippet.resourceId.videoId);
-    }
-    if (nextPageToken != null)
-      countmutex.runExclusive(() => {
-        getPlaylistsInfo(id, nextPageToken);
-      });
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-const statisticsOfVideo = () => {
-  obj = {
-    videoInPlaylist,
-    totalDurationOfVideo,
-    avgVideoDuration: Math.round(totalDurationOfVideo / videoInPlaylist),
-    totalLikeCount,
-    avgLikeCount: Math.round(totalLikeCount / videoInPlaylist),
-    __comment__: "Video length is in seconds",
+    return (
+      24 * 60 * 60 * timer[0] + 60 * 60 * timer[1] + 60 * timer[2] + timer[3]
+    );
   };
-  totalDurationOfVideo = 0;
-  totalLikeCount = 0;
-  return obj;
-};
+
+  async getVideoInfo(id) {
+    const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&fields=items(contentDetails(duration)),items(statistics(likeCount))&key=${process.env.YOUTUBE_API}`;
+
+    try {
+      const response = await axios.get(url);
+      const itm = response.data.items;
+      if (+itm.length == 0) {
+        await countmutex.runExclusive(() => {
+          this.totalVideo--;
+        });
+        return;
+      }
+
+      await apimutex.runExclusive(() => {
+        this.totalDurationOfVideo += this.getVideoTimeline(
+          itm[0].contentDetails.duration
+        );
+        if (Object.keys(itm[0].statistics).length != 0)
+          this.totalLikeCount += +itm[0].statistics.likeCount;
+        countmutex.runExclusive(() => {
+          this.totalVideo--;
+          if (this.totalVideo == 0)
+            this.mainResponse.send(this.statisticsOfVideo());
+        });
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getPlaylistsInfo(id, pageToken = null) {
+    let currentToken = "";
+    if (pageToken != null) {
+      currentToken = `pageToken=${pageToken}`;
+    }
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?${currentToken}&part=snippet&maxResults=50&playlistId=${id}&key=${process.env.YOUTUBE_API}&fields=items(snippet(resourceId)),nextPageToken,prevPageToken,pageInfo`;
+    try {
+      const response = await axios.get(url);
+      if (response.statusCode == 400) throw new Error(response.error);
+      const nextPageToken = await response.data.nextPageToken;
+      const itm = response.data.items;
+
+      if (pageToken == null)
+        countmutex.runExclusive(() => {
+          this.totalVideo = +response.data.pageInfo.totalResults;
+          this.videoInPlaylist = this.totalVideo;
+        });
+
+      for (let index = 0; index < itm.length; index++) {
+        this.getVideoInfo(itm[index].snippet.resourceId.videoId);
+      }
+      if (nextPageToken != null)
+        countmutex.runExclusive(() => {
+          this.getPlaylistsInfo(id, nextPageToken);
+        });
+    } catch (error) {
+      console.error(url);
+      throw new Error(error);
+    }
+  }
+
+  statisticsOfVideo() {
+    const videoInPlaylist = this.videoInPlaylist;
+    const totalDurationOfVideo = this.totalDurationOfVideo;
+    const avgVideoDuration = Math.round(
+      this.totalDurationOfVideo / this.videoInPlaylist
+    );
+    const totalLikeCount = this.totalLikeCount;
+    const avgLikeCount = Math.round(this.totalLikeCount / this.videoInPlaylist);
+
+    const obj = {
+      videoInPlaylist,
+      totalDurationOfVideo,
+      avgVideoDuration,
+      totalLikeCount,
+      avgLikeCount,
+      __comment__: "Video length is in seconds",
+    };
+    return obj;
+  }
+}
 
 const get = async function (req, res) {
-  mainResponse = res;
-  await getPlaylistsInfo(req.params.id);
+  let youtube = new Youtube();
+  youtube.Youtube();
+  youtube.mainResponse = res;
+  await youtube.getPlaylistsInfo(req.params.id);
 };
 
 module.exports = get;
